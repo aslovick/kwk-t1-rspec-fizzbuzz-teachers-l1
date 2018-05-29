d761b85a85ca7be73368fa8a2ccbc347ac8f48ae
Abigail Slovick <abby.slovick@gmail.com>
2018-05-29 21:43:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,14 @@ describe "FizzBuzz" do
   # Specifications, that is, specific test examples, go here.
 end
 it 'returns "FizzBuzz" when the number is divisible by 15' do
-  fizz_15 = fizzbuzz(15)
-  expect(fizz_15).to eq("FizzBuzz")# Code that actually does what we're testing and confirms the desired result
+  fizzbuzz_15 = fizzbuzz(15)
+  expect(fizzbuzz_15).to eq("FizzBuzz")# Code that actually does what we're testing and confirms the desired result
 end
 it 'returns "Fizz" when the number is divisible by 3' do
   fizz_3 = fizzbuzz(3)
   expect(fizz_3).to eq("Fizz")
 end
 it 'returns "Buzz" when the number is divisible by 5' do
-  fizz_5 = fizzbuzz(5)
-  expect(fizz_5).to eq("Buzz")
+  buzz_5 = fizzbuzz(5)
+  expect(buzz_5).to eq("Buzz")
 end
