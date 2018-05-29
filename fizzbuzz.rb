7801399e93ff0ae542c8681cf477b6b13fcbe82d
Abigail Slovick <abby.slovick@gmail.com>
2018-05-29 21:51:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,10 @@
-def fizz_buzz
-  require_relative './spec_helper.rb'
-  require_relative '../fizzbuzz.rb'
+require_relative './spec_helper.rb'
+require_relative '../fizzbuzz.rb'
+def fizzbuzz(int)
+  if int % 15 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+      "Fizz"
+  elsif int % 5 == 0
+      "Buzz"
 end
